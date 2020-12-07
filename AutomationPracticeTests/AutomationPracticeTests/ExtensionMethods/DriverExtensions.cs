55217f0ac71fc150d0d65cdf0fe1ff59642d9a34
MartinShalev <martoshalev@gmail.com>
2020-12-07 13:35:00 +0200

    Add Core folder/Add ExrensionMethods

---
+++ AutomationPracticeTests/AutomationPracticeTests/ExtensionMethods/DriverExtensions.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using SeleniumTests.Core;
+public static class DriverExtensions
+{
+    public static void ScrollUp(this WebDriver driver, int pixels)
+    {
+        ((IJavaScriptExecutor)driver.WrappedDriver).ExecuteScript($"window.scrollBy(0, {-pixels})");
+    }
+
+    public static void ScrollDown(this WebDriver driver, int pixels)
+    {
+        ((IJavaScriptExecutor)driver.WrappedDriver).ExecuteScript($"window.scrollBy(0, {pixels})");
+    }
+
+    public static WebElement ScrollToElement(this WebDriver driver, WebElement element)
+    {
+        ((IJavaScriptExecutor)driver.WrappedDriver).ExecuteScript("arguments[0].scrollIntoView(true);", element.WrappedElement);
+        return element;
+    }
+}
