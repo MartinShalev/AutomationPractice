4ef8313e9de740e4de42269bc6ca10283afe31e8
MartinShalev <martoshalev@gmail.com>
2020-12-08 17:49:17 +0200

    New tsest sets Presence of footer elements Test Sute "PresenceOfFooterSocialMediaImages"

---
+++ AutomationPracticeTests/AutomationPracticeTests/Tests/FooterTest/FooterElements/PresentsOfFooteerElements.cs
@@ -0,0 +1,59 @@
+﻿using AutomationPracticeTests.Sections;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationPracticeTests.Tests.FooterTest.FooterElements
+{
+    [TestFixture]
+    public class PresentsOfFooteerElements : BaseTest
+    {
+        private Footer _footerSection;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Initialize();
+            _footerSection = new Footer(Driver);
+            Driver.GoToUrl(_footerSection.URL);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+            {
+                TakeScreenshot(@"..\..\..\");
+            }
+            Driver.Quit();
+        }
+
+        [Test]
+        [TestCase("Women")]
+        [TestCase("Specials")]
+        [TestCase("Best sellers")]
+        [TestCase("Our stores")]
+        [TestCase("Contact us")]
+        [TestCase("Terms and conditions of use")]
+        [TestCase("About us")]
+        [TestCase("Sitemap")]
+        [TestCase("My orders")]
+        [TestCase("My credit slips")]
+        [TestCase("My addresses")]
+        [TestCase("My personal info")]
+        public void PresentsOfFooterLinkElements(string linkText)
+        {
+            Driver.ScrollToElement(_footerSection.FooterLinks(linkText));
+
+            Assert.IsTrue(_footerSection.FooterLinks(linkText).Displayed);
+        }
+
+        [Test]
+        public void PresenceOfFooterSocialMediaImages()
+        {
+
+        }
+    }
+}
