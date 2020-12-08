b9c7106b65b32c2b9d6aade85294a89de71c8f48
MartinShalev <martoshalev@gmail.com>
2020-12-08 11:33:07 +0200

    Add FooterLinksMyAccountOptionsWhenUserIsNOTLogIn Test in Footer test folder

---
+++ AutomationPracticeTests/AutomationPracticeTests/Pages/LogIn/LogInPage.cs
@@ -0,0 +1,17 @@
+﻿using SeleniumTests.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationPracticeTests.Pages.LogIn
+{
+    class LogInPage : BasePage
+    {
+        public LogInPage(WebDriver driver) 
+            : base(driver)
+        {
+        }
+
+        public string URL { get { return ""; } }
+    }
+}
--- AutomationPracticeTests/AutomationPracticeTests/Tests/FooterTest/FooterLink.cs
+++ AutomationPracticeTests/AutomationPracticeTests/Tests/FooterTest/FooterLink.cs
@@ -17,34 +17,6 @@ namespace AutomationPracticeTests.Tests.FooterTest
 
         }
 
-        [Test]
-        [TestCase("Women")]
-        [TestCase("Specials")]
-        [TestCase("Best sellers")]
-        [TestCase("Our stores")]
-        [TestCase("Contact us")]
-        [TestCase("Terms and conditions of use")]
-        [TestCase("About us")]
-        [TestCase("Sitemap")]
-        [TestCase("My orders")]
-        [TestCase("My credit slips")]
-        [TestCase("My addresses")]
-        [TestCase("My personal info")]
-
-        public void CheckFooterLinks(string linkText)
-        {
-            var urlBeforeClick = _footterSection.FooterLinks(linkText).GetAttribute("href").ToString();
-
-            _footterSection.FooterLinks(linkText).Click();
-
-            var curentURL = Driver.Url().ToString();
-
-            Assert.AreEqual(urlBeforeClick, curentURL);
-
-        }
-
-        // TODO Last 4 testCases when finished the LogIn tests !!!! 
-
         [TearDown]
         public void TearDown()
         {
@@ -54,5 +26,41 @@ namespace AutomationPracticeTests.Tests.FooterTest
             }
             Driver.Quit();
         }
+
+        [Test]
+        [TestCase("Women")]
+        [TestCase("Specials")]
+        [TestCase("Best sellers")]
+        [TestCase("Our stores")]
+        [TestCase("Contact us")]
+        [TestCase("Terms and conditions of use")]
+        [TestCase("About us")]
+        [TestCase("Sitemap")]
+
+        public void FooterLinksWhenUserIsNOTLogIn(string linkText)
+        {
+            var elementUrlBeforeClick = _footterSection.FooterLinks(linkText).GetAttribute("href").ToString();
+
+            _footterSection.FooterLinks(linkText).Click();
+
+            var curentURL = Driver.Url().ToString();
+
+            Assert.AreEqual(elementUrlBeforeClick, curentURL);
+
+        }
+        [TestCase("My orders")]
+        [TestCase("My credit slips")]
+        [TestCase("My addresses")]
+        [TestCase("My personal info")]
+        public void FooterLinksMyAccountOptionsWhenUserIsNOTLogIn(string linkText)
+        {
+
+            _footterSection.FooterLinks(linkText).Click();
+
+            var test = Driver.Url().ToString();
+
+            Assert.IsTrue(Driver.Url().Contains("authentication&back"));
+        }
+
     }
 }
