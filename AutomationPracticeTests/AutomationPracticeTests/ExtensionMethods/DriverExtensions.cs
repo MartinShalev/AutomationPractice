436a6f7e63bba9456500d3f4dd36ff6124b244c8
MartinShalev <martoshalev@gmail.com>
2020-12-14 18:03:52 +0200

    Add HoverElement Method in DriverExtensions

--- AutomationPracticeTests/AutomationPracticeTests/ExtensionMethods/DriverExtensions.cs
+++ AutomationPracticeTests/AutomationPracticeTests/ExtensionMethods/DriverExtensions.cs
@@ -18,4 +18,19 @@ public static class DriverExtensions
         ((IJavaScriptExecutor)driver.WrappedDriver).ExecuteScript("arguments[0].scrollIntoView(true);", element.WrappedElement);
         return element;
     }
+
+    public static WebElement HoverElement(this WebDriver driver, WebElement element)
+    {
+        ((IJavaScriptExecutor)driver.WrappedDriver).
+            ExecuteScript
+            (
+            "var evObj = document.createEvent('MouseEvents'); " +
+            "evObj.initMouseEvent(\"mouseover\",true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);" +
+            "arguments[0].dispatchEvent(evObj);",
+             element.WrappedElement
+             );
+
+        return element;
+
+    }
 }
